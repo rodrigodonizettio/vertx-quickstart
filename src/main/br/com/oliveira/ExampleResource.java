23a4c794b9acd26158329195ef58543f37d09684
rodrigodonizettio <rodrigodonizettio@gmail.com>
2021-01-12 07:42:25 -0300

    Using Mutiny (Uni) library to read a sample file when a request to /hello endpoint occurs
    Uni: works with async call providing 0 or 1 result.

--- src/main/br/com/oliveira/ExampleResource.java
+++ src/main/br/com/oliveira/ExampleResource.java
@@ -1,16 +1,34 @@
 package main.br.com.oliveira;
 
+import io.vertx.mutiny.core.Vertx;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.File;
 
 @Path("/hello")
 public class ExampleResource {
 
+    @Inject
+    Vertx vertx;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        vertx.fileSystem().readFile(getSampleFilePath())
+                .onItem().transform(buffer -> buffer.toString("UTF-8"))
+                .subscribe()
+                .with(
+                        content -> System.out.println("Content: " + content),
+                        err -> System.out.println("Cannot read the file: " + err.getMessage())
+                );
         return "Hello RESTEasy";
+    }
+
+    private String getSampleFilePath() {
+        return new File("../src/main/resources/sample-name.txt").getPath();
     }
 }
