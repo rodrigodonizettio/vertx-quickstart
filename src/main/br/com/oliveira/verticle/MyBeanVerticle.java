2291621fc9d0594024d00bff5a3c94909f8f527d
rodrigodonizettio <rodrigodonizettio@gmail.com>
2021-01-18 07:36:28 -0300

    Creating a Bean Verticle from AbstractVerticle super class
    Moreover,
    - The deployVerticle method does a manual verticle deployment.
    - Even Verticles aren't beans by default, the @ApplicationScoped adds
    to the class Inject support.

---
+++ src/main/br/com/oliveira/verticle/MyBeanVerticle.java
@@ -0,0 +1,27 @@
+package main.br.com.oliveira.verticle;
+
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.vertx.core.AbstractVerticle;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class MyBeanVerticle extends AbstractVerticle {
+  @ConfigProperty(name = "address")
+  String address;
+
+  @Override
+  public Uni<Void> asyncStart() {
+    return vertx.eventBus().consumer(address)
+            .handler(m -> m.replyAndForget("MyBean Verticle Greetings"))
+            .completionHandler();
+  }
+
+  private void deployVerticle() {
+    vertx.deployVerticle(this.getClass().getName());
+//    deployVerticle(MyBeanVerticle.class.getName(), ar -> { }); //Deploying verticle with options
+    vertx.deployVerticle(new MyBeanVerticle());
+//    deployVerticle(new MyBeanVerticle(), ar -> { }); //Deploying verticle with options
+  }
+}
