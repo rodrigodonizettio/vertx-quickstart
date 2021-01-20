fa0b1d47eba533d4a5d5b1876922db8d2c6146ef
rodrigodonizettio <rodrigodonizettio@gmail.com>
2021-01-20 08:00:01 -0300

    Creating a consumer method that reply with a Uni type

--- src/main/br/com/oliveira/service/GreetingService.java
+++ src/main/br/com/oliveira/service/GreetingService.java
@@ -2,15 +2,26 @@ package main.br.com.oliveira.service;
 
 import io.quarkus.vertx.ConsumeEvent;
 import io.smallrye.common.annotation.Blocking;
+import io.smallrye.mutiny.Uni;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.concurrent.Executor;
 
 @ApplicationScoped
 public class GreetingService {
+  @Inject
+  Executor executor;
+
   /** If not set, address is the fully qualified name of the bean. e.g. "main.br.com.oliveira.service.GreetingService" **/
   @ConsumeEvent
   public String consume(String name) {
     return name.toUpperCase();
+  }
+
+  @ConsumeEvent
+  public Uni<String> process(String name) {
+    return Uni.createFrom().item(() -> name.toUpperCase()).emitOn(executor);
   }
 
   /** For blocking consumer code use the parameter "blocking = true" inside @ConsumeEvent or use the annotation @Blocking **/
