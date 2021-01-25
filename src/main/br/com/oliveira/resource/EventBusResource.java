3c30b983fed02dbcedb1b5238a21549ac75955be
rodrigodonizettio <rodrigodonizettio@gmail.com>
2021-01-25 07:39:17 -0300

    Implementing an Event Bus to send messages to a specific address

---
+++ src/main/br/com/oliveira/resource/EventBusResource.java
@@ -0,0 +1,26 @@
+package main.br.com.oliveira.resource;
+
+import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.core.eventbus.EventBus;
+import io.vertx.mutiny.core.eventbus.Message;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/async")
+public class EventBusResource {
+  @Inject
+  EventBus eventBus;
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  @Path("{name}")
+  public Uni<String> greetingAddress1(@PathParam String name) {
+    return eventBus.<String>request("greeting-address1", name)
+            .onItem().transform(Message::body);
+  }
+}
