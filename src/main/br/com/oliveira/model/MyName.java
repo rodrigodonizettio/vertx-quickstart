6d62145c106ce4e2397ff3af2c1190d65b41b5f4
rodrigodonizettio <rodrigodonizettio@gmail.com>
2021-01-26 08:03:33 -0300

    Creating a simple model to exercise CODEC (Serialize/Deserialize) use in Quarkus

---
+++ src/main/br/com/oliveira/model/MyName.java
@@ -0,0 +1,13 @@
+package main.br.com.oliveira.model;
+
+public class MyName {
+  private String name;
+
+  public MyName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
