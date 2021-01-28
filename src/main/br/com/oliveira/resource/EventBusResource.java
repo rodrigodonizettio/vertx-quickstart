8feffd495e7ee471a4f6682811ab464782b25fab
rodrigodonizettio <rodrigodonizettio@gmail.com>
2021-01-28 07:21:02 -0300

    Sending a Serialized object with specific Codec through EventBus

--- src/main/br/com/oliveira/resource/EventBusResource.java
+++ src/main/br/com/oliveira/resource/EventBusResource.java
@@ -1,8 +1,10 @@
 package main.br.com.oliveira.resource;
 
 import io.smallrye.mutiny.Uni;
+import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.mutiny.core.eventbus.EventBus;
 import io.vertx.mutiny.core.eventbus.Message;
+import main.br.com.oliveira.codec.MyNameCodec;
 import main.br.com.oliveira.model.MyName;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
@@ -30,6 +32,15 @@ public class EventBusResource {
   @Path("/general-codec/{name}")
   public Uni<String> sendSerializedDeserializedMessageAndExpectReply(@PathParam String name) {
     return eventBus.<String>request("greetingaddress5", new MyName(name))
+            .onItem().transform(Message::body);
+  }
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  @Path("/specific-codec/{name}")
+  public Uni<String> sendSerializedDeserializedMessageUsingSpecificCodecAndExpectReply(@PathParam String name) {
+    return eventBus.<String>request("greetingaddress6", name,
+            new DeliveryOptions().setCodecName(MyNameCodec.class.getName()))
             .onItem().transform(Message::body);
   }
 
